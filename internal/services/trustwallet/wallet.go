006aa5655f7f287e2c08cef4e75424362a1072b8
Yuri Surbashev <yurist@insport.net>
2021-08-31 15:33:09 +0800

    Refactor TW service wallet functions.

--- internal/services/trustwallet/wallet.go
+++ internal/services/trustwallet/wallet.go
@@ -5,19 +5,23 @@ package trustwallet
 // #include <TrustWalletCore/TWMnemonic.h>
 // #include <TrustWalletCore/TWHDWallet.h>
 import "C"
-import "unsafe"
+import (
+	"unsafe"
+
+	"github.com/yurist-85/gosignit/internal/services/trustwallet/types"
+)
 
 // MnemonicIsValid checks if given string is a valid mnemonic phrase.
 func MnemonicIsValid(mnemonic string) bool {
-	cValid := C.TWMnemonicIsValid(TWStringCreateWithGoString(mnemonic))
+	cValid := C.TWMnemonicIsValid(types.TWStringCreateWithGoString(mnemonic))
 
 	return bool(cValid)
 }
 
 // WalletCreateWithMnemonic creates wallet with a given mnemonic phrase.
 func WalletCreateWithMnemonic(mnemonic string) (unsafe.Pointer, error) {
-	str := TWStringCreateWithGoString(mnemonic)
-	empty := TWStringCreateWithGoString("")
+	str := types.TWStringCreateWithGoString(mnemonic)
+	empty := types.TWStringCreateWithGoString("")
 	wallet, err := C.TWHDWalletCreateWithMnemonic(str, empty)
 
 	return unsafe.Pointer(wallet), err
@@ -31,10 +35,18 @@ func WalletDelete(wallet unsafe.Pointer) error {
 	return err
 }
 
-// WalletGetKeyForCoin retrieves wallet key for a given coin
+// WalletGetKeyForCoin retrieves wallet key for a given coin.
 func WalletGetKeyForCoin(wallet, coin unsafe.Pointer) (unsafe.Pointer, error) {
 	ct := *(*C.enum_TWCoinType)(coin)
 	key := C.TWHDWalletGetKeyForCoin((*C.struct_TWHDWallet)(wallet), ct)
 
 	return unsafe.Pointer(key), nil
+}
+
+// WalletGetAddressForCoin generates address for a given wallet and coin.
+func WalletGetAddressForCoin(wallet, coin unsafe.Pointer) (string, error) {
+	ct := *(*C.enum_TWCoinType)(coin)
+	addr := C.TWHDWalletGetAddressForCoin((*C.struct_TWHDWallet)(wallet), ct)
+
+	return types.TWStringGoString(addr), nil
 }
--- internal/services/trustwallet/wallet_test.go
+++ internal/services/trustwallet/wallet_test.go
@@ -9,11 +9,21 @@ import (
 
 type TestWalletSuite struct {
 	suite.Suite
+	mnemonic string
+}
+
+type testCase struct {
+	coin    string
+	address string
 }
 
 func TestWallet(t *testing.T) {
 	t.Parallel()
 	suite.Run(t, new(TestWalletSuite))
+}
+
+func (t *TestWalletSuite) SetupTest() {
+	t.mnemonic = "observe drum fault concert analyst old short plunge loan essence symbol invite"
 }
 
 func (t *TestWalletSuite) TestMnemonicIsValid_fail() {
@@ -22,12 +32,12 @@ func (t *TestWalletSuite) TestMnemonicIsValid_fail() {
 }
 
 func (t *TestWalletSuite) TestMnemonicIsValid_ok() {
-	valid := MnemonicIsValid("observe drum fault concert analyst old short plunge loan essence symbol invite")
+	valid := MnemonicIsValid(t.mnemonic)
 	t.Require().True(valid)
 }
 
 func (t *TestWalletSuite) TestWalletCreateWithMnemonic_ok() {
-	wallet, err := WalletCreateWithMnemonic("observe drum fault concert analyst old short plunge loan essence symbol invite")
+	wallet, err := WalletCreateWithMnemonic(t.mnemonic)
 	t.Require().NoError(err)
 	t.Require().NotEqual(unsafe.Pointer(nil), wallet)
 }
@@ -39,10 +49,49 @@ func (t *TestWalletSuite) TestWalletCreateWithMnemonic_fail() {
 }
 
 func (t *TestWalletSuite) TestWalletDelete_ok() {
-	wallet, err := WalletCreateWithMnemonic("observe drum fault concert analyst old short plunge loan essence symbol invite")
+	wallet, err := WalletCreateWithMnemonic(t.mnemonic)
 	t.Require().NoError(err)
 	t.Require().NotNil(wallet)
 	t.Require().NotEqual(unsafe.Pointer(nil), wallet)
+
 	errDel := WalletDelete(wallet)
 	t.Require().NoError(errDel)
+}
+
+func (t *TestWalletSuite) TestWalletGetKeyForCoin_ok() {
+	wallet, err := WalletCreateWithMnemonic(t.mnemonic)
+	t.Require().NoError(err)
+	t.Require().NotNil(wallet)
+	t.Require().NotEqual(unsafe.Pointer(nil), wallet)
+
+	coin, err := CoinInfoByIdString("bitcoin")
+	t.Require().NoError(err)
+	t.Require().NotNil(coin)
+
+	key, err := WalletGetKeyForCoin(wallet, unsafe.Pointer(&coin.CoinType))
+	t.Require().NoError(err)
+	t.Require().NotNil(key)
+}
+
+func (t *TestWalletSuite) TestWalletGetAddressForCoin_ok() {
+	wallet, err := WalletCreateWithMnemonic(t.mnemonic)
+	t.Require().NoError(err)
+	t.Require().NotNil(wallet)
+	t.Require().NotEqual(unsafe.Pointer(nil), wallet)
+
+	cases := []testCase{
+		{"bitcoin", "bc1q0tum480myw4kn6y94n5phvkpxqnducaqqmxp76"},
+		{"ethereum", "0xd7B3500AB740d84F4A4e7aa36ecBAb343020A23B"},
+	}
+
+	for _, c := range cases {
+		coin, err := CoinInfoByIdString(c.coin)
+		t.Require().NoError(err)
+		t.Require().NotNil(coin)
+
+		addr, err := WalletGetAddressForCoin(wallet, unsafe.Pointer(&coin.CoinType))
+		t.Require().NoError(err)
+		t.Require().NotNil(addr)
+		t.Require().Equal(c.address, addr)
+	}
 }
